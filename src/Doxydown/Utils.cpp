bc0d579f034d7bd1414e6a943dd62c4f42c63e3f
Matus Novak <email@matusnovak.com>
2019-10-31 22:22:05 +0100

    [skip travis] Updating circleci config

--- src/Doxydown/Utils.cpp
+++ src/Doxydown/Utils.cpp
@@ -14,11 +14,7 @@
 
 static const std::regex ANCHOR_REGEX("_[a-z0-9]{34,67}$");
 
-#if defined(__APPLE__) || defined(__MINGW32__)
-    static const auto US_LOCALE = std::locale("en_US");
-#else
-    static const auto US_LOCALE = std::locale("en_US.utf8");
-#endif
+static const auto US_LOCALE = std::locale("en_US");
 
 static std::string replaceAll(std::string str, const std::string& from, const std::string& to) {
     size_t pos = 0;
