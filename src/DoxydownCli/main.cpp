0dc6308f51de87b16b39f3fa1b47d0337930bc36
Matus Novak <email@matusnovak.com>
2019-10-31 22:06:01 +0100

    Fixing circleci and enums templating

--- src/DoxydownCli/main.cpp
+++ src/DoxydownCli/main.cpp
@@ -8,6 +8,7 @@
 #include <Doxydown/TemplateDefaultLoader.hpp>
 #include <Doxydown/TemplateFolderLoader.hpp>
 #include <Doxydown/Doxygen.hpp>
+#include <Doxydown/Log.hpp>
 
 static argagg::parser argparser {{
     {
@@ -24,25 +25,19 @@ static argagg::parser argparser {{
     },
     {
         "config", {"-c", "--config"},
-        "Path to a config json file. You can generate this json file with --generate-config", 1
+        "Optional path to a config json file.", 1
     },
     {
         "templates", {"-t", "--templates"},
-        "Path to a folder with templates. This folder does not have to have all "
-        "templates! You can generate them with --generate-templates. "
-        "Any template file in this folder will override the default one.", 1
+        "Optional path to a folder with templates.", 1
     },
     {
         "generate-config", {"--generate-config"},
-        "Generate config file given a path to json file (this json file will be created)", 1
+        "Generate config file given a path to the destination json file", 1
     },
     {
         "generate-templates", {"--generate-templates"},
-        "Generate template files given a path to folder (this folder must exist). "
-        "These generated templates are the default templates used internally. You can "
-        "override them with your custom ones by changing these templates generated. "
-        "If you do not provide template, for example to kind_namespace, the default "
-        "one will be used!", 1
+        "Generate template files given a path to folder (folder must exist).", 1
     }
 }};
 
@@ -83,15 +78,15 @@ int main(const int argc, char* argv[]) {
             return EXIT_SUCCESS;
         }
 
-        if (args["config"]) {
-            loadConfig(config, args["config"].as<std::string>());
-        }
-
         if (args["output"]) {
             if (!args["input"]) {
                 std::cerr << "You need to provide input path!" << std::endl;
                 std::cerr << argparser;
                 return EXIT_FAILURE;
+            }
+
+            if (args["config"]) {
+                loadConfig(config, args["config"].as<std::string>());
             }
 
             config.outputDir = args["output"].as<std::string>();
@@ -113,11 +108,14 @@ int main(const int argc, char* argv[]) {
             generator.printIndex(doxygen, "classes", INDEX_CLASS_FILTER);
             generator.printIndex(doxygen, "namespaces", INDEX_NAMESPACES_FILTER);
             generator.printIndex(doxygen, "groups", INDEX_MODULES_FILTER);
+        } else {
+            std::cerr << argparser;
+            return EXIT_FAILURE;
         }
 
         return EXIT_SUCCESS;
     } catch (std::exception& e) {
-        std::cerr << e.what() << std::endl;
+        Log::e(e.what());
         return EXIT_FAILURE;
     }
 }
