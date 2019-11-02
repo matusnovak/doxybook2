2c3379a8a3c066082665eb8420cde884d28e0998
Matus Novak <email@matusnovak.com>
2019-11-02 00:28:35 +0000

    Updates

---
+++ hugo-book/search.min.550648d6948cb3da120ef76653c667797e19dfea4905e90fc151595b6b863d92.js
@@ -0,0 +1,5 @@
+'use strict';(function(){const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');input.addEventListener('focus',init);input.addEventListener('keyup',search);function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/doxydown/hugo-book/flexsearch.min.js');loadScript('/doxydown/hugo-book/search-data.min.275fe32236f0527fb83e80516dfbf818c0cdb350420289388e81cf1d7a4c2bea.js',function(){input.required=false;search();});}
+function search(){while(results.firstChild){results.removeChild(results.firstChild);}
+if(!input.value){return;}
+const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
+function loadScript(src,callback){const script=document.createElement('script');script.defer=true;script.async=false;script.src=src;script.onload=callback;document.head.appendChild(script);}})();
