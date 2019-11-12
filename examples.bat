f26aee8cc219d0c4aee6060474635a9370c2092a
Matus Novak <email@matusnovak.com>
2019-11-12 21:07:00 +0100

    Fixing build and deploy scripts

--- examples.bat
+++ examples.bat
@@ -1,39 +1,39 @@
 "build\src\Doxybook2Cli\Debug\doxybook2.exe" ^
     --input example/doxygen/xml ^
     --output example/mkdocs-readthedocs/docs ^
-    --config example/mkdocs-readthedocs/.doxybook2/config.json
+    --config example/mkdocs-readthedocs/.doxybook/config.json
     
 "build\src\Doxybook2Cli\Debug\doxybook2.exe" ^
     --input example/doxygen/xml ^
     --output example/mkdocs-material/docs ^
-    --config example/mkdocs-material/.doxybook2/config.json
+    --config example/mkdocs-material/.doxybook/config.json
 
 "build\src\Doxybook2Cli\Debug\doxybook2.exe" ^
     --input example/doxygen/xml ^
     --output example/mkdocs-bootstrap/docs ^
-    --config example/mkdocs-bootstrap/.doxybook2/config.json
+    --config example/mkdocs-bootstrap/.doxybook/config.json
 
 "build\src\Doxybook2Cli\Debug\doxybook2.exe" ^
     --input example/doxygen/xml ^
     --output example/hugo-learn/content ^
-    --config example/hugo-learn/.doxybook2/config.json ^
-    --templates example/hugo-learn/.doxybook2/templates
+    --config example/hugo-learn/.doxybook/config.json ^
+    --templates example/hugo-learn/.doxybook/templates
 
 "build\src\Doxybook2Cli\Debug\doxybook2.exe" ^
     --input example/doxygen/xml ^
     --output example/hugo-book/content ^
-    --config example/hugo-book/.doxybook2/config.json ^
-    --templates example/hugo-book/.doxybook2/templates
+    --config example/hugo-book/.doxybook/config.json ^
+    --templates example/hugo-book/.doxybook/templates
 
 "build\src\Doxybook2Cli\Debug\doxybook2.exe" ^
     --input example/doxygen/xml ^
     --output example/vuepress ^
-    --config example/vuepress/.doxybook2/config.json
+    --config example/vuepress/.doxybook/config.json
 
 "build\src\Doxybook2Cli\Debug\doxybook2.exe" ^
     --input example/doxygen/xml ^
     --output example/gitbook ^
-    --config example/gitbook/.doxybook2/config.json
+    --config example/gitbook/.doxybook/config.json
 
 "build\src\Doxybook2Cli\Debug\doxybook2.exe" ^
     --input example/doxygen/xml ^
