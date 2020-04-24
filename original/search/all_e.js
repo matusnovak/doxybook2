3c374d97ffa3db2a447afc5649394ef4eed2709e
Matus Novak <email@matusnovak.com>
2020-04-24 19:30:38 +0000

    Updates

--- original/search/all_e.js
+++ original/search/all_e.js
@@ -2,8 +2,8 @@ var searchData=
 [
   ['setcallback_79',['setCallback',['../classEngine_1_1Audio_1_1AudioBuffer.html#a9e2d6ba7577091ff59e68f2d520e9174',1,'Engine::Audio::AudioBuffer']]],
   ['setcallback2_80',['setCallback2',['../classEngine_1_1Audio_1_1AudioBuffer.html#aa86b2b3bdc3ff9825d4e273f855adabe',1,'Engine::Audio::AudioBuffer']]],
-  ['setdata_81',['setData',['../classEngine_1_1Audio_1_1AudioBuffer.html#a560600772af8cde2fd5dc45e1780789c',1,'Engine::Audio::AudioBuffer::setData(const TypedAudioData &amp;data)'],['../classEngine_1_1Audio_1_1AudioBuffer.html#a457036b75735061701ccf1adbad747ce',1,'Engine::Audio::AudioBuffer::setData(const TypedAudioData[] data, size_t size)']]],
-  ['setdatamultiple_82',['setDataMultiple',['../classEngine_1_1Audio_1_1AudioBuffer.html#ae2638a0252ef80a2d3cb3c5932df3c83',1,'Engine::Audio::AudioBuffer']]],
+  ['setdata_81',['setData',['../classEngine_1_1Audio_1_1AudioBuffer.html#a560600772af8cde2fd5dc45e1780789c',1,'Engine::Audio::AudioBuffer::setData(const TypedAudioData &amp;data)'],['../classEngine_1_1Audio_1_1AudioBuffer.html#a5a9f408cc4d73ea1956277e0e189f8f0',1,'Engine::Audio::AudioBuffer::setData(const TypedAudioData data[], size_t size)']]],
+  ['setdatamultiple_82',['setDataMultiple',['../classEngine_1_1Audio_1_1AudioBuffer.html#a51ef075612af06edcb53d3c9b6c4fc7c',1,'Engine::Audio::AudioBuffer']]],
   ['size_83',['size',['../classEngine_1_1Utils_1_1ArrayView.html#a3079597de87ffeffe28d3cbf144ea8d3',1,'Engine::Utils::ArrayView']]],
   ['someglobalfunc_84',['someGlobalFunc',['../Engine_8hpp.html#ae7d52a33d99eaf0d779f82301ce5432f',1,'Engine.hpp']]],
   ['stop_85',['stop',['../classEngine_1_1Audio_1_1AudioBuffer.html#a387243170ec71207123ae326c7dabd1a',1,'Engine::Audio::AudioBuffer']]],
