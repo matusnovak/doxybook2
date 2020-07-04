e13ba113630620bf889140a57a8aa02271235257
Matus Novak <email@matusnovak.com>
2020-07-04 14:13:14 +0200

    Fix find rang

--- modules/FindRang.cmake
+++ modules/FindRang.cmake
@@ -6,7 +6,7 @@ mark_as_advanced(FORCE Rang_INCLUDE_DIR)
 if(NOT TARGET Rang::Rang)
     add_library(Rang::Rang INTERFACE IMPORTED)
     set_target_properties(Rang::Rang PROPERTIES
-        INTERFACE_INCLUDE_DIRECTORIES ${Dirent_INCLUDE_DIR})
+        INTERFACE_INCLUDE_DIRECTORIES ${Rang_INCLUDE_DIR})
 endif()
 
-find_package_handle_standard_args(Rang DEFAULT_MSG Dirent_INCLUDE_DIR)
+find_package_handle_standard_args(Rang DEFAULT_MSG Rang_INCLUDE_DIR)
