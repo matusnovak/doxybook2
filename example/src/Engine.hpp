516b54f53953129655985f9333ec8e9415cdefcf
Matus Novak <email@matusnovak.com>
2021-01-30 20:16:29 +0100

    Added block formula tests

--- example/src/Engine.hpp
+++ example/src/Engine.hpp
@@ -118,6 +118,28 @@ First Header  | Second Header | Third Header
 Content Cell  | Content Cell  | Content Cell
 Content Cell  | Content Cell  | Content Cell
 
+## Inline formula
+
+Please see mkdocs-readthedocs example at
+https://matusnovak.github.io/doxybook2/mkdocs-readthedocs/
+to see a working example using mkdocs + pymdownx.arithmatex
+
+The distance between \f$(x_1,y_1)\f$ and \f$(x_2,y_2)\f$ is
+  \f$\sqrt{(x_2-x_1)^2+(y_2-y_1)^2}\f$.
+
+## Block formula
+
+\f[
+    |I_2|=\left| \int_{0}^T \psi(t)
+             \left\{
+                u(a,t)-
+                \int_{\gamma(t)}^a
+                \frac{d\theta}{k(\theta,t)}
+                \int_{a}^\theta c(\xi)u_t(\xi,t)\,d\xi
+             \right\} dt
+          \right|
+  \f]
+
 */
 
 //-----------------------------------------------------------
--- include/Doxybook/Config.hpp
+++ include/Doxybook/Config.hpp
@@ -1,101 +1,118 @@
 #pragma once
 
+#include "Enums.hpp"
+
 #include <filesystem>
 #include <string>
 #include <vector>
 
 namespace Doxybook2 {
-    struct Config {
-        // Where to store the output files?
-        std::string outputDir;
+struct Config {
+    // Where to store the output files?
+    std::string outputDir;
 
-        // Generate extra JSON for each rendered template
-        bool debugTemplateJson{false};
+    // Generate extra JSON for each rendered template
+    bool debugTemplateJson{false};
 
-        // Put all stuff into categorized folders or everything into destination folder?
-        bool useFolders{true};
+    // Put all stuff into categorized folders or everything into destination folder?
+    bool useFolders{true};
 
-        // Sort alphabetically
-        bool sort{false};
+    // Sort alphabetically
+    bool sort{false};
 
-        // Copy images from the Doxygen xml dir?
-        bool copyImages{true};
+    // Copy images from the Doxygen xml dir?
+    bool copyImages{true};
 
-        // Where to copy images
-        std::string imagesFolder{"images"};
+    // Where to copy images
+    std::string imagesFolder{"images"};
 
-        // Convert all refids (including folder names) into lowercase?
-        bool linkLowercase{false};
+    // Convert all refids (including folder names) into lowercase?
+    bool linkLowercase{false};
 
-        // Should we put the class, namespace, modules, and files indexes
-        // into their respective folders? (Hugo/Learn) needs that!
-        bool indexInFolders{false};
+    // Should we put the class, namespace, modules, and files indexes
+    // into their respective folders? (Hugo/Learn) needs that!
+    bool indexInFolders{false};
 
-        // If mainpage (indexpage) is present, should it be located
-        // in the outputDir folder or in outpurDir/folderRelatedPagesName folder?
-        bool mainPageInRoot{false};
+    // If mainpage (indexpage) is present, should it be located
+    // in the outputDir folder or in outpurDir/folderRelatedPagesName folder?
+    bool mainPageInRoot{false};
 
-        // Name of the mainpage (indexpage)
-        std::string mainPageName{"indexpage"};
+    // What characters should be escaped in markdown?
+    std::string markdownEscape{"_*#`\\[{<"};
 
-        // What is the base URL to prepend all links with?
-        std::string baseUrl{""};
+    // Name of the mainpage (indexpage)
+    std::string mainPageName{"indexpage"};
 
-        // What should the file end with?
-        std::string fileExt{"md"};
+    // What is the base URL to prepend all links with?
+    std::string baseUrl{""};
 
-        // What should the link end with?
-        std::string linkSuffix{".md"};
+    // What should the file end with?
+    std::string fileExt{"md"};
 
-        // What are the folder names?
-        std::string folderGroupsName{"Modules"};
-        std::string folderClassesName{"Classes"};
-        std::string folderFilesName{"Files"};
-        std::string folderRelatedPagesName{"Pages"};
-        std::string folderNamespacesName{"Namespaces"};
-        std::string folderExamplesName{"Examples"};
+    // What should the link end with?
+    std::string linkSuffix{".md"};
 
-        // What are the names of the index files?
-        std::string indexGroupsName{"index_groups"};
-        std::string indexClassesName{"index_classes"};
-        std::string indexFilesName{"index_files"};
-        std::string indexRelatedPagesName{"index_pages"};
-        std::string indexNamespacesName{"index_namespaces"};
-        std::string indexExamplesName{"index_examples"};
+    // What are the folder names?
+    std::string folderGroupsName{"Modules"};
+    std::string folderClassesName{"Classes"};
+    std::string folderFilesName{"Files"};
+    std::string folderRelatedPagesName{"Pages"};
+    std::string folderNamespacesName{"Namespaces"};
+    std::string folderExamplesName{"Examples"};
 
-        // What templates to use for each kind?
-        std::string templateKindClass{"kind_class"};
-        std::string templateKindStruct{"kind_class"};
-        std::string templateKindUnion{"kind_class"};
-        std::string templateKindInterface{"kind_class"};
-        std::string templateKindNamespace{"kind_nonclass"};
-        std::string templateKindGroup{"kind_nonclass"};
-        std::string templateKindFile{"kind_file"};
-        std::string templateKindDir{"kind_file"};
-        std::string templateKindPage{"kind_page"};
-        std::string templateKindExample{"kind_page"};
+    // What are the names of the index files?
+    std::string indexGroupsName{"index_groups"};
+    std::string indexClassesName{"index_classes"};
+    std::string indexFilesName{"index_files"};
+    std::string indexRelatedPagesName{"index_pages"};
+    std::string indexNamespacesName{"index_namespaces"};
+    std::string indexExamplesName{"index_examples"};
 
-        // What templates to use for indexes?
-        std::string templateIndexClasses{"index_classes"};
-        std::string templateIndexNamespaces{"index_namespaces"};
-        std::string templateIndexGroups{"index_groups"};
-        std::string templateIndexFiles{"index_files"};
-        std::string templateIndexRelatedPages{"index_pages"};
-        std::string templateIndexExamples{"index_examples"};
+    // What templates to use for each kind?
+    std::string templateKindClass{"kind_class"};
+    std::string templateKindStruct{"kind_class"};
+    std::string templateKindUnion{"kind_class"};
+    std::string templateKindInterface{"kind_class"};
+    std::string templateKindNamespace{"kind_nonclass"};
+    std::string templateKindGroup{"kind_nonclass"};
+    std::string templateKindFile{"kind_file"};
+    std::string templateKindDir{"kind_file"};
+    std::string templateKindPage{"kind_page"};
+    std::string templateKindExample{"kind_page"};
 
-        // What should the title be in the index pages?
-        std::string indexClassesTitle{"Classes"};
-        std::string indexNamespacesTitle{"Namespaces"};
-        std::string indexGroupsTitle{"Modules"};
-        std::string indexFilesTitle{"Files"};
-        std::string indexRelatedPagesTitle{"Pages"};
-        std::string indexExamplesTitle{"Examples"};
+    // What templates to use for indexes?
+    std::string templateIndexClasses{"index_classes"};
+    std::string templateIndexNamespaces{"index_namespaces"};
+    std::string templateIndexGroups{"index_groups"};
+    std::string templateIndexFiles{"index_files"};
+    std::string templateIndexRelatedPages{"index_pages"};
+    std::string templateIndexExamples{"index_examples"};
 
-        // Which source files are allowed to be included in the output? (empty => all)
-        std::vector<std::string> filesFilter{};
+    // What should the title be in the index pages?
+    std::string indexClassesTitle{"Classes"};
+    std::string indexNamespacesTitle{"Namespaces"};
+    std::string indexGroupsTitle{"Modules"};
+    std::string indexFilesTitle{"Files"};
+    std::string indexRelatedPagesTitle{"Pages"};
+    std::string indexExamplesTitle{"Examples"};
+
+    // Which source files are allowed to be included in the output? (empty => all)
+    std::vector<std::string> filesFilter{};
+
+    // Folder to generate.
+    std::vector<FolderCategory> foldersToGenerate{
+        FolderCategory::MODULES, FolderCategory::CLASSES,    FolderCategory::FILES,
+        FolderCategory::PAGES,   FolderCategory::NAMESPACES, FolderCategory::EXAMPLES,
     };
 
-    void loadConfig(Config& config, const std::filesystem::path& path);
-    void loadConfigData(Config& config, const std::string& src);
-    void saveConfig(Config& config, const std::filesystem::path& path);
+    // Formulas (Latex)
+    std::string formulaInlineStart{"\\("};
+    std::string formulaInlineEnd{"\\)"};
+    std::string formulaBlockStart{"\\["};
+    std::string formulaBlockEnd{"\\]"};
+};
+
+void loadConfig(Config& config, const std::filesystem::path& path);
+void loadConfigData(Config& config, const std::string& src);
+void saveConfig(Config& config, const std::filesystem::path& path);
 } // namespace Doxybook2
