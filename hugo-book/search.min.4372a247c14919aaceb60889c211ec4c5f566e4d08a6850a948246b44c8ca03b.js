81b9c1c8fc11e9fea341d1245f48ea518808308d
matusnovak <matusnovak@users.noreply.github.com>
2021-09-19 21:58:03 +0000

    deploy: 5c53103978dc3b9158120f99116455f7729627da

---
+++ hugo-book/search.min.4372a247c14919aaceb60889c211ec4c5f566e4d08a6850a948246b44c8ca03b.js
@@ -0,0 +1,5 @@
+'use strict';(function(){const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');input.addEventListener('focus',init);input.addEventListener('keyup',search);function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/doxybook2/hugo-book/flexsearch.min.js');loadScript('/doxybook2/hugo-book/search-data.min.12327cc8e8232405b2564fee2d5886a031d01327ee23d4f624fb90463fefb77a.js',function(){input.required=false;search();});}
+function search(){while(results.firstChild){results.removeChild(results.firstChild);}
+if(!input.value){return;}
+const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
+function loadScript(src,callback){const script=document.createElement('script');script.defer=true;script.async=false;script.src=src;script.onload=callback;document.head.appendChild(script);}})();
